1793a8ac8880d8702ce369ec06ce7ba10c29b1ad
kurehajime <xiidec@gmail.com>
2023-04-08 11:47:21 +0900

    匿名化

--- go.mod
+++ go.mod
@@ -11,6 +11,9 @@ require (
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/swag v0.19.5 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/ikawaha/kagome-dict v1.0.9 // indirect
+	github.com/ikawaha/kagome-dict/ipa v1.0.10 // indirect
+	github.com/ikawaha/kagome/v2 v2.9.2 // indirect
 	github.com/invopop/yaml v0.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"net/http"
-	"strings"
+	"regexp"
+	"unicode/utf8"
 
+	ipa "github.com/ikawaha/kagome-dict/ipa"
+	"github.com/ikawaha/kagome/v2/tokenizer"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -17,7 +20,7 @@ func (h Server) Query(ctx echo.Context) error {
 	}
 
 	return ctx.JSON(http.StatusOK, Res{
-		Answer: strings.ToUpper(req.Query),
+		Answer: anony(req.Query, false),
 	})
 }
 func (h Server) OpenApi(ctx echo.Context) error {
@@ -27,6 +30,75 @@ func (h Server) OpenApi(ctx echo.Context) error {
 func (h Server) AiPlugin(ctx echo.Context) error {
 	ctx.Response().Header().Set("Content-Type", "text/json; charset=utf-8")
 	return ctx.File("ai-plugin.json")
+}
+
+var t *tokenizer.Tokenizer
+
+func init() {
+	var err error
+	t, err = tokenizer.New(ipa.Dict())
+	if err != nil {
+		panic(err)
+	}
+}
+
+func anony(text string, single bool) string {
+	tokens := t.Tokenize(text)
+	var rText string
+	var IniCount int
+	for j := 0; j < len(tokens); j++ {
+		tk := tokens[j]
+		ft := tk.Features()
+		if len(ft) > 7 {
+			if ft[2] == "人名" && ft[1] == "固有名詞" {
+				if IniCount == 0 {
+					rText += Word2initial(ft[7])
+				} else if IniCount == 1 && single == false {
+					rText += "・"
+					rText += Word2initial(ft[7])
+				}
+				IniCount++
+			} else {
+				rText += tk.Surface
+				IniCount = 0
+			}
+		} else if len(ft) > 0 {
+			rText += tk.Surface
+			IniCount = 0
+		}
+	}
+	return rText
+}
+
+func Word2initial(kana string) string {
+	r, _ := utf8.DecodeRune([]byte(kana))
+	ini := string(r)
+
+	ini = regexp.MustCompile("[アァ]").ReplaceAllString(ini, "A")
+	ini = regexp.MustCompile("[イィ]").ReplaceAllString(ini, "I")
+	ini = regexp.MustCompile("[ウゥ]").ReplaceAllString(ini, "U")
+	ini = regexp.MustCompile("[エェ]").ReplaceAllString(ini, "E")
+	ini = regexp.MustCompile("[オォ]").ReplaceAllString(ini, "O")
+	ini = regexp.MustCompile("[カキクケコ]").ReplaceAllString(ini, "K")
+	ini = regexp.MustCompile("[サシスセソ]").ReplaceAllString(ini, "S")
+	ini = regexp.MustCompile("[タツテト]").ReplaceAllString(ini, "T")
+	ini = regexp.MustCompile("[チ]").ReplaceAllString(ini, "T")
+	ini = regexp.MustCompile("[ナニヌネノ]").ReplaceAllString(ini, "N")
+	ini = regexp.MustCompile("[ハヒヘホ]").ReplaceAllString(ini, "H")
+	ini = regexp.MustCompile("[フ]").ReplaceAllString(ini, "F")
+	ini = regexp.MustCompile("[マミムメモ]").ReplaceAllString(ini, "M")
+	ini = regexp.MustCompile("[ヤユヨ]").ReplaceAllString(ini, "Y")
+	ini = regexp.MustCompile("[ラリルレロ]").ReplaceAllString(ini, "R")
+	ini = regexp.MustCompile("[ワヲ]").ReplaceAllString(ini, "W")
+	ini = regexp.MustCompile("[ン]").ReplaceAllString(ini, "N")
+	ini = regexp.MustCompile("[ガギグゲゴ]").ReplaceAllString(ini, "G")
+	ini = regexp.MustCompile("[ザズゼゾ]").ReplaceAllString(ini, "Z")
+	ini = regexp.MustCompile("[ダヂヅデド]").ReplaceAllString(ini, "D")
+	ini = regexp.MustCompile("[ジ]").ReplaceAllString(ini, "J")
+	ini = regexp.MustCompile("[パピプペポ]").ReplaceAllString(ini, "P")
+	ini = regexp.MustCompile("[バビブベボ]").ReplaceAllString(ini, "B")
+
+	return ini
 }
 
 func main() {
